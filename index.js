2f002c4b70c2e558652946d6ef35c870f6019469
Prajwal-sk041 <prajwalsk041@gmail.com>
2025-02-09 00:30:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>🚀 Auto-Deployment Works! (Updated)</h1>');
+  res.end('<h1>👋 Hello! Now you are able to see the updated page. 🚀</h1>');
 });
 
 server.listen(80, () => {
