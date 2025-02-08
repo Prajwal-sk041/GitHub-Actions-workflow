d52e854caa36f739798f3f291773fe0378e2ccfb
Prajwal-sk041 <prajwalsk041@gmail.com>
2025-02-09 00:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World! Your Node.js app is running on AWS EC2!');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>🚀 Auto-Deployment Works! (Updated)</h1>');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(80, () => {
+  console.log('Server running on port 80');
 });
